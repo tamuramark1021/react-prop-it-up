d41d592c85596249494ed024f8128e12e08a0200
unknown <tamuramark1021@gmail.com>
2020-05-06 15:17:26 -0600

    prop it up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PersonCard 
+          firstName="Mark"
+          lastName="Tamura"
+          age={33}
+          hairColor="black"/>
+        <PersonCard 
+          firstName="Isiah"
+          lastName="Fletcher"
+          age={25}
+          hairColor="sandy blonde"/>
+        <PersonCard 
+          firstName="Kevin"
+          lastName="Udink"
+          age={45}
+          hairColor="brown"/>
       </header>
     </div>
   );
---
+++ src/components/PersonCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component{
+    constructor(props) {
+        super(Component)
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <h3>Age: {this.props.age}</h3>
+                <h3>Hair Color: {this.props.hairColor}</h3>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
